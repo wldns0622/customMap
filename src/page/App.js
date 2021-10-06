62a3047549ec746c4db4e4a968a6dbd37e1aca3b
wldns0622 <wldns12378@gmail.com>
2021-10-06 18:20:37 +0900

    feat: map drag 구현

--- src/page/App.js
+++ src/page/App.js
@@ -3,26 +3,52 @@ import styled from 'styled-components';
 
 import mapSource from '../assets/images/map.png';
 
-const Container = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+
 
 const Map = styled.section`
   width: 1024px;
   height: 768px;
   overflow: hidden;
+  position: relative;
+
+  img {
+    position: absolute;
+  }
 `;
+
+const handleMoveMap = (event) => {
+  const map = event.target;
+  const mapRect = map.getBoundingClientRect()
+  console.dir(map);
+  let shiftX = event.clientX - mapRect.left;
+  let shiftY = event.clientY - mapRect.top;
+
+  const handleClickMap = (pageX, pageY) => {
+    map.style.left = (pageX - shiftX) + 'px';
+    map.style.top = (pageY - shiftY) + 'px';
+  }
+
+  const handleMoveMap = (event) => {
+    handleClickMap(event.pageX, event.pageY);
+  }
+
+  const handleDropMap = () => {
+    map.removeEventListener('mousemove', handleMoveMap);
+    map.removeEventListener('mouseup', handleDropMap);
+  }
+
+  handleClickMap(event.pageX, event.pageY);
+
+  map.addEventListener('mousemove', handleMoveMap);
+  map.addEventListener('mouseup', handleDropMap);
+  
+}
 
 function App() {
   return (
-    <Container>
-      <Map>
-        <img src={mapSource} alt="지도" />
-      </Map>
-    </Container>
+    <Map>
+      <img src={mapSource} alt="지도" onDragStart={(event)=> event.preventDefault()} onMouseDown={handleMoveMap}/>
+    </Map>
   );
 }
 
